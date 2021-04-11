a0e7406e331b81776e8f6e77204fa4edc71152c6
Wesley Earl Stander <wesleys@bbd.co.za>
2021-04-11 16:11:24 +0200

    Basic input working

--- src/main/java/com/hive/rpg/App.java
+++ src/main/java/com/hive/rpg/App.java
@@ -8,6 +8,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        boolean isRunning;
+        int framesPerSecond = 60;
+        int timePerLoop = 1000000000 / framesPerSecond;
+        Screen screen;
+        screen = new Screen(500,500);
+        isRunning = true;
+
+        while(isRunning) {
+            long startTime = System.nanoTime();
+
+            screen.inputHandler.processInput(); //60 fps input processing (actual game can be a manual game loop)
+
+            long endTime = System.nanoTime();
+
+            long sleepTime = timePerLoop - (endTime - startTime);
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime / 1000000);
+                } catch (InterruptedException e) {
+                    isRunning = false;
+                }
+            }
+        }
     }
 }

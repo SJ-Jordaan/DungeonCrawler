e4103ad3ce58eae32a18688e42ec2cf61bab4c63
Zandolor <steven.j.jordaan2000@gmail.com>
2021-04-14 23:06:24 +0200

    Add map output to screen ui

--- src/main/java/com/hive/rpg/Screen.java
+++ src/main/java/com/hive/rpg/Screen.java
@@ -4,16 +4,30 @@ import javax.swing.*;
 
 import asciiPanel.AsciiPanel;
 import com.hive.rpg.InputHandler;
+import com.hive.rpg.map.Map;
 
 public class Screen extends JFrame {
+
     public InputHandler inputHandler;
+    private AsciiPanel terminal;
+
     public Screen(int width, int height) {
         inputHandler = new InputHandler();
         super.addKeyListener(inputHandler);
-        AsciiPanel terminal = new AsciiPanel(width, height);
+        this.terminal = new AsciiPanel(width, height);
         super.add(terminal);
         super.setSize(width,height);
         super.setVisible(true);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void outputMap(int MAP_WIDTH, int MAP_HEIGHT, Map map) {
+        for (int i = 0; i < MAP_WIDTH; i++) {
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                int[] coord = {i,y};
+                this.terminal.write(map.getTile(coord).getGlyph(), i, y);
+            }
+        }
+        this.terminal.repaint();
     }
 }

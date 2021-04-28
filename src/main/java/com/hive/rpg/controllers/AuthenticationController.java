3b9c4b7429f0e6ed6212e173a7187a86a4eb59fb
Zandolor <steven.j.jordaan2000@gmail.com>
2021-04-28 08:10:17 +0200

    temp merge commit

--- src/main/java/com/hive/rpg/controllers/AuthenticationController.java
+++ src/main/java/com/hive/rpg/controllers/AuthenticationController.java
@@ -1,14 +1,13 @@
 package com.hive.rpg.controllers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.Console;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hive.rpg.models.User;
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
 
 public class AuthenticationController {
     public User user;
